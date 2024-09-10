6f61c461c3f073c71fef308fa95305adf1a889d6
thiagogilcamargo <thiagogilcmargo@gmail.com>
2024-09-09 22:37:00 -0300

    Adicionar arquivos ao índice e configurar .gitignore

---
+++ Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Personal_Assist_API.Models;
+
+namespace Personal_Assist_API.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Servico> Servicos { get; set; }
+        public DbSet<Suporte> Suportes { get; set; }
+    }
+}
---
+++ Models/Servico.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Personal_Assist_API.Models
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+
+        public Empresa Empresa { get; set; }
+    }
+}
